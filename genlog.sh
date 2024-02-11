5160ab692f8952da3b01544b774e57066e1071cb
Varun Thakore <vrnthakore@gmail.com>
2024-02-12 01:03:40 +0530

    Update logs

--- genlog.sh
+++ genlog.sh
@@ -8,6 +8,6 @@ echo "Generating values for $1 iteration"
 command ./target/release/gen_values $1 > /dev/null
 echo "Values generation complete!"
 echo "Generating output logs for $1 iteration"
-command ./target/release/examples/por $1 > ./logs/por/output_$1.txt
-command ./target/release/examples/pnc $1 > ./logs/pnc/output_$1.txt
+command ./target/release/examples/por $1 > ./logs/zkpor/output_$1.txt
+command ./target/release/examples/pnc $1 > ./logs/zkpnc/output_$1.txt
 echo "See logs directory for output files"
