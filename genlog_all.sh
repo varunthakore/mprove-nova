57150cdf6486e485097dd82c10529c67463ba98f
Varun Thakore <vrnthakore@gmail.com>
2024-02-14 16:59:36 +0530

    Update genlog

--- genlog_all.sh
+++ genlog_all.sh
@@ -6,8 +6,8 @@ do
     command ./target/release/gen_values $i > /dev/null
     echo "Values generation complete!"
     echo "Generating output logs for $i iteration"
-    command ./target/release/examples/por $i > ./logs/zkpor/output_$i.txt
-    command ./target/release/examples/pnc $i > ./logs/zkpnc/output_$i.txt
+    command ./target/release/examples/por $i > ./logs/por/output_$i.txt
+    command ./target/release/examples/pnc $i > ./logs/pnc/output_$i.txt
     echo "Sleeping for 1 mins to give the CPU a break"
     sleep 60
 done
