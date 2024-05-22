e3faf24b686a2bc86f2f81e7648d53df673b213c
Varun Thakore <vrnthakore@gmail.com>
2024-05-23 00:20:50 +0530

    Update log script

--- genlog.sh
+++ genlog.sh
@@ -7,7 +7,11 @@ fi
 echo "Generating values for $1 iteration"
 command ./target/release/gen_values $1 > /dev/null
 echo "Values generation complete!"
+echo "Sleeping for 1 mins to give the CPU a break"
+sleep 60
 echo "Generating output logs for $1 iteration"
 command ./target/release/examples/por $1 > ./logs/por/output_$1.txt
+echo "Sleeping for 1 mins to give the CPU a break"
+sleep 60
 command ./target/release/examples/pnc $1 > ./logs/pnc/output_$1.txt
 echo "See logs directory for output files"
--- genlog_all.sh
+++ genlog_all.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-iters=(10000 15000 20000)
+iters=(1000 3000 5000 7000 10000 15000 20000)
 for i in "${iters[@]}"
 do
     echo "Generating values for $i iteration"
     command ./target/release/gen_values $i > /dev/null
     echo "Values generation complete!"
+    echo "Sleeping for 1 mins to give the CPU a break"
+    sleep 60
     echo "Generating output logs for $i iteration"
     command ./target/release/examples/por $i > ./logs/por/output_$i.txt
+    echo "Sleeping for 1 mins to give the CPU a break"
+    sleep 60
     command ./target/release/examples/pnc $i > ./logs/pnc/output_$i.txt
     echo "Sleeping for 1 mins to give the CPU a break"
     sleep 60
